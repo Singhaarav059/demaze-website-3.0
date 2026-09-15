8403eecfdfd7a22a3119ba31d0332900dd771a53
singhaarav059 <singhaarav059@gmail.com>
2026-09-15 19:24:56 +0530

    fix(viewport): calibrate hero fit for short viewports and fix journey timeline CSS grid

--- js/main.js
+++ js/main.js
@@ -1560,8 +1560,8 @@ function initScrollRevealStorytelling() {
       }
     });
   }, {
-    threshold: 0.12,
-    rootMargin: '0px 0px -30px 0px'
+    threshold: 0.05,
+    rootMargin: '40px 0px -10px 0px'
   });
 
   elements.forEach(el => {
